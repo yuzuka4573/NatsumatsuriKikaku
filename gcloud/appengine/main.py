64e584ab23cc384e669c7b01ce00ac2d4dea162b
i544c <leo@i544c.me>
2020-07-11 19:18:53 +0900

    Add GCE client

--- gcloud/appengine/main.py
+++ gcloud/appengine/main.py
@@ -2,21 +2,41 @@ import os
 
 import discord
 from discord.ext import commands
+import googleapiclient.discovery
 
-description = '''サーバを操作するためのBotだよ！'''
+# GCP
+compute = googleapiclient.discovery.build('compute', 'v1')
+project = os.getenv('GOOGLE_CLOUD_PROJECT')
+zone = 'us-central1-a'
+instance = 'instance-1'
 
+# Bot
+description = """サーバを操作するためのBotだよ！"""
 bot = commands.Bot(command_prefix='?', description=description)
 
-@bot.command(description='サーバの状態を確認する')
-async def get(ctx):
-    await ctx.send('[dry-run] サーバの状態は次の通り！')
+@bot.command(name='status')
+async def _get(ctx):
+    """✅ サーバの状態を確認する"""
+    result = compute.instances() \
+        .get(project=project, zone=zone, instance=instance) \
+        .execute()
+    status = result['status']
+    await ctx.send(f'✅ サーバの状態: {status}')
 
-@bot.command(description='サーバを起動する')
+@bot.command()
 async def start(ctx):
-    await ctx.send('[dry-run] サーバの起動を受け付けたよ！ちょっと待ってね...')
+    """⏰ サーバを起動する"""
+    compute.instances() \
+        .start(project=project, zone=zone, instance=instance) \
+        .execute()
+    await ctx.send('⏰ サーバの起動を受け付けたよ！ちょっと待ってね...')
 
-@bot.command(description='サーバを停止する')
+@bot.command()
 async def stop(ctx):
-    await ctx.send('[dry-run] サーバの停止を受け付けたよ！ちょっと待ってね...')
+    """💤 サーバを停止する"""
+    compute.instances() \
+        .stop(project=project, zone=zone, instance=instance) \
+        .execute()
+    await ctx.send('💤 サーバの停止を受け付けたよ！ちょっと待ってね...')
 
 bot.run(os.environ['DISCORD_BOT_TOKEN'])
